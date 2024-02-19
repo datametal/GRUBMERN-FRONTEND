a5f244eb79f21fb211786a82de4216d33b91a724
Daryl <daryl@datametal.io>
2024-02-19 02:38:44 -0500

    feat(routing): add initial routes
    added default and test user-profile routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
+import AppRoutes from './AppRoutes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router></Router>
+    <Router>
+      <AppRoutes />{' '}
+    </Router>
   </React.StrictMode>,
 )
